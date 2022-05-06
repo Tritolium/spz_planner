48efe11d3d58827561f3da471df7e706c12c2b76
Tritolium <paracrafta@gmail.com>
2022-05-06 09:40:23 +0200

    Update Overview.js

--- src/components/dateadministration/Overview.js
+++ src/components/dateadministration/Overview.js
@@ -54,7 +54,7 @@ const Overview = () => {
     const filterChange = useCallback((e) => {
         setFilter(e.target.value)
         console.log(filter)
-    }, [])
+    }, [filter])
 
     return(
         <>
@@ -93,7 +93,7 @@ const Filter = (props) => {
 
     const onChange = useCallback((e) => {
         props.onChange(e)
-    }, [])
+    }, [props])
 
     return(
         <select onChange={onChange}>
