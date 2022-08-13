c40a0b543e577cdf2aa8513929bd02ef3f89036d
Tritolium <paracrafta@gmail.com>
2022-08-13 14:37:02 +0200

    optimized css

--- src/components/dateplanner/overview/Overview.styled.js
+++ src/components/dateplanner/overview/Overview.styled.js
@@ -4,4 +4,5 @@ export const StyledOverview = styled.div`
     display: flex;
     flex-direction: column;
     align-content: center;
+    margin: auto;
 `
--- src/modules/components/menu/Menu.js
+++ src/modules/components/menu/Menu.js
@@ -16,7 +16,7 @@ const Menu = ({open, navigate, auth_level, setOpen}) => {
     return(
         <StyledMenu open={open}>
             {auth_level > 0 ? <Button type='button' id='main_button_0' font_size={button_size} onClick={nav}>Anwesenheiten</Button> : <></>}
-            {auth_level > 2 ? <Button type='button' id='main_button_1' font_size={button_size} onClick={nav}>Abwesenheiten</Button> : <></>}
+            {/*auth_level > 2 ? <Button type='button' id='main_button_1' font_size={button_size} onClick={nav}>Abwesenheiten</Button> : <></>*/}
             {auth_level > 1 ? <Button type='button' id='main_button_2' font_size={button_size} onClick={nav}>Mitglieder</Button> : <></>}
             {auth_level > 0 ? <Button type='button' id='main_button_3' font_size={button_size} onClick={nav}>Termine</Button> : <></>}
         </StyledMenu>        
