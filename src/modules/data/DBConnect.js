3de2617153c8f31297c01651c1790dd0a612f6d0
Tritolium <paracrafta@gmail.com>
2022-09-03 14:58:44 +0200

    cleanup, local dev

--- src/modules/data/DBConnect.js
+++ src/modules/data/DBConnect.js
@@ -76,18 +76,14 @@ const getEvents = async (filter) => {
     let events = new Array(0)
     let token = cookies.get('api_token')
 
-    if (process.env.NODE_ENV !== 'production') {
-        events = mockDB.events
-    } else {
-        let response = await fetch(`${host}/api/event.php?filter=${filter}&api_token=${token}`, {method: "GET"})
+    let response = await fetch(`${host}/api/event.php?filter=${filter}&api_token=${token}`, {method: "GET"})
 
-        switch(response.status) {
-            case 200:
-                events = await response.json()
-                break
-            default:
-                break
-        }
+    switch(response.status) {
+        case 200:
+            events = await response.json()
+            break
+        default:
+            break
     }
     
     return events
@@ -120,7 +116,7 @@ const newEvent = async (type, location, date, begin, departure, leave_dep, accep
     
     let token = cookies.get('api_token')
     
-    let response = await fetch(`/api/event.php?api_token=${token}`, {
+    let response = await fetch(`${host}/api/event.php?api_token=${token}`, {
         method: "POST",
         body: JSON.stringify({
             Type: type,
@@ -270,7 +266,7 @@ const getAttendences = async (all) => {
             break
         }
     } else {
-        let response = await fetch("/api/attendence.php?api_token=" + token, {
+        let response = await fetch(`${host}/api/attendence.php?api_token=${token}`, {
             method: "GET"
         })
         switch(response.status){
@@ -286,58 +282,50 @@ const getAttendences = async (all) => {
 
 const updateAttendences = async (changes) => {
     let token = cookies.get('api_token')
-    if(process.env.NODE_ENV !== 'production'){
-        // TODO set in MockDB
-    } else {
-        fetch("/api/attendence.php?api_token=" + token, {
-            method: "PUT",
-            body: JSON.stringify(changes)
-        }).then(
-            res => {
-                if(res.status === 200)
-                    alert('Angaben übernommen')
-                else
-                    alert('Ein Fehler ist aufgetreten')
-            }
-        )
-    }
+    
+    fetch(`${host}/api/attendence.php?api_token=${token}`, {
+        method: "PUT",
+        body: JSON.stringify(changes)
+    }).then(
+        res => {
+            if(res.status === 200)
+                alert('Angaben übernommen')
+            else
+                alert('Ein Fehler ist aufgetreten')
+        }
+    )
 }
 
 const getMissingFeedback = async () => {
-    if(process.env.NODE_ENV !== 'production'){
-        return []
-    } else {
-        let token = cookies.get('api_token')
-        let response = await fetch("/api/attendence.php?api_token=" + token + "&missing", {
-            method: "GET"
-        })
-        switch(response.status){
-        case 200:
-            let json = await response.json()
-            return json
-        default:
-            return
-        }
+    
+    let token = cookies.get('api_token')
+
+    let response = await fetch(`${host}/api/attendence.php?api_token=${token}&missing`, {
+        method: "GET"
+    })
+    switch(response.status){
+    case 200:
+        let json = await response.json()
+        return json
+    default:
+        return
     }
 }
 
 const getEval = async () => {
-    if(process.env.NODE_ENV !== 'production'){
-        return mockDB.evaluation
-    } else {
-        let token = cookies.get('api_token')
-        let response = await fetch("/api/eval.php?api_token=" + token + "&events", {
-            method: "GET"
-        })
-        switch(response.status){
-        case 200:
-            let json = await response.json()
-            return json
-        default:
-            return
-        }
-    }
     
+    let token = cookies.get('api_token')
+
+    let response = await fetch(`${host}/api/eval.php?api_token=${token}&events`, {
+        method: "GET"
+    })
+    switch(response.status){
+    case 200:
+        let json = await response.json()
+        return json
+    default:
+        return
+    }    
 }
 
 /**
@@ -345,99 +333,86 @@ const getEval = async () => {
  */
 
 export const getAbsence = async (absence_id) => {
-    if(process.env.NODE_ENV !== 'production'){
+    
+    let token = cookies.get('api_token')
+
+    let response = await fetch(`/api/absence.php?id=${absence_id}&api_token=${token}`, {
+        method: 'GET'
+    })
+    switch(response.status){
+    case 200:
+        let json = await response.json()
+        return json
+    default:
         return
-    } else {
-        let token = cookies.get('api_token')
-        let response = await fetch('/api/absence.php?id=' + absence_id + '&api_token=' + token, {
-            metod: 'GET'
-        })
-        switch(response.status){
-        case 200:
-            let json = await response.json()
-            return json
-        default:
-            return
-        }
     }
 }
 
 export const getAbsences = async (filter) => {
-    if(process.env.NODE_ENV !== 'production'){
-        return mockDB.absenceOwn
-    } else {
-        let token = cookies.get('api_token')
-        let response = await fetch('/api/absence.php?api_token=' + token + '&filter='+ filter, {
-            method: 'GET'
-        })
-        switch(response.status){
-        case 200:
-            let json = await response.json()
-            return json
-        default:
-            return
-        }
+
+    let token = cookies.get('api_token')
+
+    let response = await fetch(`${host}/api/absence.php?api_token=${token}&filter=${filter}`, {
+        method: 'GET'
+    })
+    switch(response.status){
+    case 200:
+        let json = await response.json()
+        return json
+    default:
+        return
     }
 }
 
 export const updateAbsence = async (absence_id, member_id, from, until, info) => {
-    if(process.env.NODE_ENV !== 'production') {
-        return
-    } else {
-        let token = cookies.get('api_token')
-        let response = await fetch('/api/absence.php?api_token=' + token + '&id=' + absence_id, {
-            method: 'PUT',
-            body: JSON.stringify({
-                Member_ID: member_id,
-                From: from,
-                Until: until,
-                Info: info
-            })
+
+    let token = cookies.get('api_token')
+    let response = await fetch(`/api/absence.php?api_token=${token}&id=${absence_id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            Member_ID: member_id,
+            From: from,
+            Until: until,
+            Info: info
         })
-        switch(response.status){
-        case 200:
-            return
-        default:
-            alert('ein Fehler ist aufgetreten')
-        }
+    })
+    switch(response.status){
+    case 200:
+        return
+    default:
+        alert('ein Fehler ist aufgetreten')
     }
 }
 
 export const newAbsence = async (from, until, info) => {
-    if(process.env.NODE_ENV !== 'production'){
-        return
-    } else {
-        let token = cookies.get('api_token')
-        let response = await fetch('/api/absence.php?api_token=' + token, {
-            method: 'POST',
-            header: {
-                "content-type": "application/json"
-            },
-            body: JSON.stringify({
-                From: from,
-                Until: until,
-                Info: info
-            })
+
+    let token = cookies.get('api_token')
+
+    let response = await fetch(`${host}/api/absence.php?api_token=${token}`, {
+        method: 'POST',
+        header: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            From: from,
+            Until: until,
+            Info: info
         })
-        switch(response.status){
-        case 201:
-            alert('Angaben übernommen')
-            break
-        default:
-            alert('ein Fehler ist aufgetreten')
-            break
-        }
+    })
+    switch(response.status){
+    case 201:
+        alert('Angaben übernommen')
+        break
+    default:
+        alert('ein Fehler ist aufgetreten')
+        break
     }
 }
 
 export const deleteAbsence = async (absence_id) => {
-
-    if(process.env.NODE_ENV !== 'production'){
-        return
-    }
     
     let token = cookies.get('api_token')
-    let response = await fetch(`/api/absence.php?api_token=${token}&id=${absence_id}`, {
+    let response = await fetch(`${host}/api/absence.php?api_token=${token}&id=${absence_id}`, {
         method: 'DELETE'
     })
     switch(response.status){
