2ef1418c7933d8a6e0dc8c14bafd6b631a51151a
Tritolium <paracrafta@gmail.com>
2023-09-05 08:59:52 +0200

    add different color themes

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,46 @@ export const theme = {
     primaryLight: '#fff',
     /*primaryHover: 'rgb(0, 122, 255)',*/
     primaryHover: '#78849c',
+    blue: '#24b9d0',
+    blueRGB: 'rgb(32, 185, 208)',
+    red: 'fe423e',
+    redRGB: 'rgb(254, 66, 62)',
+    yellow: 'ffa11c',
+    yellowRGB: 'rgb(255, 161, 28)',
+    green: '00bd00',
+    greenRGB: 'rgb(0, 189, 37)',
+    mobile: '530px',
+    medium: '1007px'
+}
+
+export const theme1 = {
+    primaryDark: '#282c34',
+    primaryLight: '#e5e4e2',
+    primaryHover: '#78849c',
+    blue: '#748386',
+    blueRGB: 'rgb(116, 131, 134)',
+    red: 'ef3e36',
+    redRGB: 'rgb(239, 62, 54)',
+    yellow: 'faa916',
+    yellowRGB: 'rgb(250, 169, 22)',
+    green: '00bd00',
+    greenRGB: 'rgb(0, 189, 0)',
+    mobile: '530px',
+    medium: '1007px'
+}
+
+export const theme2 = {
+    primaryDark: '#282c34',
+    primaryLight: '#e5e4e2',
+    primaryHover: '#78849c',
+    blue: '#7c90a0',
+    blueRGB: 'rgb(124, 144, 160)',
+    red: '9a031e',
+    redRGB: 'rgb(154, 3, 30)',
+    yellow: 'ffba08',
+    yellowRGB: 'rgb(255, 186, 8)',
+    green: '47a025',
+    greenRGB: 'rgb(71, 160, 37)',
     mobile: '530px',
     medium: '1007px'
 }
