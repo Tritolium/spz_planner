646672e3fd7ba20e69e545978b03170b1797cc3c
Tritolium <paracrafta@gmail.com>
2023-09-19 11:55:17 +0200

    cleanup clothing, makes add in easier
    (cherry picked from commit e62fca50a098f9f586f131041e07c35d709fa732)

--- src/modules/components/clothing/Clothing.js
+++ src/modules/components/clothing/Clothing.js
@@ -1,8 +1,11 @@
+import blank from '../../../icons/blank_old.png'
 import polo from '../../../icons/polo.png'
 import polod from '../../../icons/polod.png'
 import shirt from '../../../icons//shirt.png'
 import suit from '../../../icons//suit.png'
 import cow from '../../../icons/cow.png'
+
+export const clothingStyles = 5
 
 export const Clothing = ({ clothing }) => {
 
@@ -29,6 +32,38 @@ export const Clothing = ({ clothing }) => {
     }
 
     return(
-        icon ? <img src={icon} alt="Uniform" /> : <></>
+        icon ? <img src={icon} alt="Uniform"/> : <></>
+    )
+}
+
+export const ClothingInput = ({ clothing, onClick }) => {
+
+    let icon
+
+    switch(parseInt(clothing)){
+    case 0:
+        icon = blank
+        break
+    case 1:
+        icon = polo
+        break
+    case 2:
+        icon = polod
+        break
+    case 3:
+        icon = shirt
+        break
+    case 4:
+        icon = suit
+        break
+    case 5:
+        icon = cow
+        break
+    default:
+        break
+    }
+
+    return(
+        icon ? <img src={icon} alt="Uniform" onClick={onClick}/> : <></>
     )
 }
