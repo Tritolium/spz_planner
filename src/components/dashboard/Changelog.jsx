f650b3f8d25956fa34217d365e5cc814646d0cff
paracrafta@gmail.com <pog@iesy.com>
2024-01-15 12:00:28 +0100

    changelog: parse changes from json
    (cherry picked from commit 8a0c59c4aeb29e6986c845d5d406fd91820c46b5)

--- src/components/dashboard/Changelog.jsx
+++ src/components/dashboard/Changelog.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react"
+import { Fragment, useState } from "react"
 import { StyledChangelog } from "./Dashboard.styled"
 import Button from "../../modules/components/button/Button"
+
+import changeData from "./logs.json"
 
 export const Changelog = ({ read, version }) => {
     const [clicked, setClicked] = useState(false)
@@ -13,48 +15,46 @@ export const Changelog = ({ read, version }) => {
     return(
         <StyledChangelog id="changelog">
             {!(read || clicked) ? 
-                <>
-                    <h2 className="New">Neu in {version}:</h2>
-                    <ul>
-                        <li>
-                            <i>Startseite/Anwesenheiten: </i>
-                            Auf der Startseite und unter Anwesenheiten können jetzt für freigeschaltete Termine Partner mit angegeben werden. Das soll die Planung für Busse etc. erleichtern.
-                        </li>
-                        <li>
-                            <i>Startseite: </i>
-                            Am Ende der Startseite wird eine kleine Nutzungsstatistik angezeigt.
-                        </li>
-                        <li>
-                            <i>Benachrichtigungen: </i>
-                            Benachrichtigungen können jetzt auch für die neue Terminkategorie "Sonstige Termine" aktiviert/deaktiviert werden. Siehe Einstellungen.
-                        </li>
-                        <li>
-                            <i>Termine: </i>
-                            Die Termine können jetzt auch nach Kategorien gefiltert werden. Vorher passierte dies nur über den Titel Üben/Probe.
-                        </li>
-                    </ul>
-                    Darüber hinaus wurden einge Dinge im Hintergrund angepasst, damit die App in Zukunft noch besser funktioniert.
-                    Kleine Aussicht für die Zukunft: Passwörter werden demnächst notwendig, damit z.B. Bildergalerien nur für Mitglieder sichtbar sind. Wer die App bereits installiert hat, wird davon wenig mitbekommen, da ein Login nicht jedes Mal notwendig ist.
-                    <br/>
-                    Einige neue Funktionen werden daher auch nur für installierte Apps verfügbar sein.
-                    <div className="Previous">
-                        <h2>Neu in v0.11:</h2>
-                        <ul>
-                            <li>
-                                <i>Startseite: </i>
-                                Für die Termine auf der Startseite können weitere Informationen hinterlegt werden. Einfach auf den Termin tippen, um das Fenster zu öffnen.<br/>
-                                Außerdem werden die Termine jetzt in drei Kategorien eingeteilt und angezeigt: Proben, Auftritte und sonstige Termine.
-                            </li>
-                            <li>
-                                <i>Allgemein: </i>
-                                Benachrichtigungen können an- & abgeschaltet werden, entweder über die Glocke, oder über die Einstellungen. Dort ist auch die Unterscheidung zwischen Üben und Auftritt möglich.
-                            </li>
-                        </ul>
-                    </div>
-                </>
+                changeData.map((change, index) => {
+                    const major = change.version.split(".")[0] === version.split(".")[0]
+                    const ispatch = change.version.split(".")[1] === version.split(".")[1]
+                    return (
+                        <ChangelogVersion key={`changelog_${index}`} change={change} ispatch={ispatch && major} />
+                    )
+                })
                 : <></>
             }
             {!(read || clicked) ? <Button onClick={onClick}>Änderungen verbergen</Button> : <></>}
         </StyledChangelog>
+    )
+}
+
+const ChangelogVersion= ({ change, ispatch }) => {    
+    return(
+        <div className={ispatch ? "Current" : "Previous"}>
+            <h2>{change.version}</h2>
+            <ul>
+                {change.entries?.map((entry, index) => {
+                    return(
+                        <ChangelogEntry key={`${change.version}_${index}`}entry={entry} />
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+const ChangelogEntry = ({ entry }) => {
+    return(
+        <li>
+            <h3>{entry.page}</h3>
+            {entry.changes?.map((change, index) => {
+                return(<Fragment key={index}>
+                    {index !== 0 ? <br /> : <></>}
+                    <span key={index}>{change}</span>
+                </Fragment>
+                )
+            })}
+        </li>
     )
 }
