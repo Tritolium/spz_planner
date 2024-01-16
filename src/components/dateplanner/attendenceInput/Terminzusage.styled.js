a8c88ce7ef9cdc5a640219cd71f3a5c057e4ab7e
paracrafta@gmail.com <pog@iesy.com>
2024-01-16 11:45:14 +0100

    attendenceInput: Terminzusage: add loading animation
    Users have to wait for the server response. Animate the loading time

--- src/components/dateplanner/attendenceInput/Terminzusage.styled.js
+++ src/components/dateplanner/attendenceInput/Terminzusage.styled.js
@@ -22,4 +22,13 @@ export const StyledTerminzusage = styled.div`
         height: 100%;
         width: 100%;
     }
+
+    .LoadingSpinner {
+        animation: spin 1s linear infinite;
+
+        @keyframes spin {
+            0% {transform: rotate(0deg);}
+            100% {transform: rotate(360deg);}
+        }
+    }
 `
