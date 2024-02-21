e088db9ef81f0b55c2cdd9f2d6316dd18b707627
Tritolium <paracrafta@gmail.com>
2024-02-21 10:17:41 +0100

    Clothing: add new suit icon

--- src/modules/components/icons/Clothing.js
+++ src/modules/components/icons/Clothing.js
@@ -2,11 +2,12 @@ import blank from '../../../icons/blank_old.png'
 import polo from '../../../icons/polo.png'
 import polod from '../../../icons/polod.png'
 import shirt from '../../../icons//shirt.png'
-import suit from '../../../icons//suit.png'
+import suit_1 from '../../../icons//suit.png'
+import suit_2 from '../../../icons/suit_2.png'
 import cow from '../../../icons/cow.png'
 import pants from '../../../icons/pants.png'
 
-export const clothingStyles = 7
+export const clothingStyles = 8
 
 export const Clothing = ({ clothing }) => {
 
@@ -23,13 +24,16 @@ export const Clothing = ({ clothing }) => {
         icon = shirt
         break
     case 4:
-        icon = suit
+        icon = suit_1
         break
     case 5:
         icon = cow
         break
     case 6:
         icon = pants
+        break
+    case 7:
+        icon = suit_2
         break
     default:
         break
@@ -58,13 +62,16 @@ export const ClothingInput = ({ clothing, onClick }) => {
         icon = shirt
         break
     case 4:
-        icon = suit
+        icon = suit_1
         break
     case 5:
         icon = cow
         break
     case 6:
         icon = pants
+        break
+    case 7:
+        icon = suit_2
         break
     default:
         break
