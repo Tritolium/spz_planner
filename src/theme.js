93e72ed953cf84e409785c66a82b651eea905e60
Tritolium <paracrafta@gmail.com>
2024-02-27 12:25:11 +0100

    theme: add dark red

--- src/theme.js
+++ src/theme.js
@@ -10,6 +10,8 @@ export const theme0 = {
     yellowRGB: 'rgb(255, 161, 28)',
     green: '#00bd00',
     greenRGB: 'rgb(0, 189, 37)',
+    darkred: '#7b2d26',
+    darkredRGB: 'rgb(123, 45, 38)',
     mobile: '530px',
     medium: '1007px'
 }
@@ -26,6 +28,8 @@ export const theme1 = {
     yellowRGB: 'rgb(250, 169, 22)',
     green: '#00bd00',
     greenRGB: 'rgb(0, 189, 0)',
+    darkred: '#7b2d26',
+    darkredRGB: 'rgb(123, 45, 38)',
     mobile: '530px',
     medium: '1007px'
 }
@@ -42,6 +46,8 @@ export const theme2 = {
     yellowRGB: 'rgb(255, 186, 8)',
     green: '#47a025',
     greenRGB: 'rgb(71, 160, 37)',
+    darkred: '#7b2d26',
+    darkredRGB: 'rgb(123, 45, 38)',
     mobile: '530px',
     medium: '1007px'
 }
@@ -58,6 +64,8 @@ export const theme3 = {
     yellowRGB: 'rgb(255, 202, 58)',
     green: '#a7c957',
     greenRGB: 'rgb(167, 201, 87)',
+    darkred: '#7b2d26',
+    darkredRGB: 'rgb(123, 45, 38)',
     mobile: '530px',
     medium: '1007px'
 }
@@ -74,6 +82,8 @@ export const theme4 = {
     yellowRGB: 'rgb(64, 8, 35))',
     green: '#67b424',
     greenRGB: 'rgb(103, 180, 36)',
+    darkred: '#7b2d26',
+    darkredRGB: 'rgb(123, 45, 38)',
     mobile: '530px',
     medium: '1007px'
 }
