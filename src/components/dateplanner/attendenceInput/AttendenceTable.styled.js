1903ead65976fc975fcf00a08632bf425e22c51c
Tritolium <paracrafta@gmail.com>
2024-06-06 17:20:20 +0200

    AttendenceTable: change design to fix rendering bug on webkit
    (cherry picked from commit ae00edd9c3a6e5ab225e339ffad296e89e19d88c)

---
+++ src/components/dateplanner/attendenceInput/AttendenceTable.styled.js
@@ -0,0 +1,99 @@
+import styled from "styled-components";
+
+export const StyledAttendenceTable = styled.div`
+    display: grid;
+`
+
+export const StyledEvent = styled.div`
+    display: grid;
+    grid-template-columns: auto 64px 64px;
+    align-items: center;
+
+    border-top: 1px solid ${({theme}) => theme.primaryLight};
+    
+    .event_type {
+        grid-column-start: 1;
+        grid-row-start: 1;
+        padding: 5px;
+        text-align: center;
+    }
+
+    .event_date {
+        grid-column-start: 1;
+        grid-row-start: 2;
+        padding: 5px;
+        text-align: center;
+    }
+
+    .event_day {
+        grid-column-start: 1;
+        grid-row-start: 3;
+        padding: 5px;
+        text-align: center;
+    }
+
+    .Terminzusage {
+        grid-column-start: 2;
+        grid-row-start: 1;
+        grid-row-end: 4;
+        justify-self: end;
+    }
+
+    .PlusOne {
+        grid-column-start: 3;
+        grid-row-start: 1;
+        grid-row-end: 4;
+        justify-self: end;
+    }
+`
+
+export const StyledMultiEvent = styled.div`
+    display: grid;
+    grid-template-columns: auto auto [icon1 icon1];
+    align-items: center;
+
+    border-top: 1px solid ${({theme}) => theme.primaryLight};
+
+    img {
+        max-height: 64px;
+        grid-column-start: 1;
+        grid-row-start: 1;
+        grid-row-end: 4;
+        justify-self: start;
+    }
+    
+    .event_type {
+        grid-column-start: 2;
+        grid-row-start: 1;
+        padding: 5px;
+        text-align: center;
+    }
+
+    .event_date {
+        grid-column-start: 2;
+        grid-row-start: 2;
+        padding: 5px;
+        text-align: center;
+    }
+
+    .event_day {
+        grid-column-start: 2;
+        grid-row-start: 3;
+        padding: 5px;
+        text-align: center;
+    }
+
+    .Terminzusage {
+        grid-column-start: 3;
+        grid-row-start: 1;
+        grid-row-end: 4;
+        justify-self: end;
+    }
+
+    .PlusOne {
+        grid-column-start: 4;
+        grid-row-start: 1;
+        grid-row-end: 4;
+        justify-self: end;
+    }
+`
