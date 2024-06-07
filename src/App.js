3081fd9b371552c159b10fa33f4c0ced8803d949
Tritolium <paracrafta@gmail.com>
2024-06-07 08:24:44 +0200

    Planer v0.13.5

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const Scoreboard = lazy(() => import('./components/scoreboard/Scoreboard'))
 const StyledApp = lazy(() => import('./App.styled'))
 
 // - ${preval`module.exports = new Date().toISOString()`}
-export const version = "v0.13.4"
+export const version = "v0.13.5"
 
 const App = () => {
 
