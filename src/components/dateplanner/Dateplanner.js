02ae11d141615631dec14a16753fa1a99f3fb181
Tritolium <paracrafta@gmail.com>
2024-09-26 14:39:47 +0200

    Dateplanner: refactor

--- src/components/dateplanner/Dateplanner.js
+++ src/components/dateplanner/Dateplanner.js
@@ -8,90 +8,52 @@ import AbsenceInput from './absenceInput/AbsenceInput'
 const AttendenceInput = lazy(() => import('./attendenceInput/Termineingabe'))
 const Overview = lazy(() => import('./overview/Overview'))
 
-const dates = [
-    {
-        Type: "FT",
-        Location: "Oelinghauserheide",
-        Date: "14. Mai 2022",
-        Day_of_Week: "Samstag"
-    },
-    {
-        Type: "SF",
-        Location: "Ennest",
-        Date: "17. Juli 2022",
-        Day_of_Week: "Sonntag"
-    }
-]
-
 const Dateplanner = (props) => {
 
     const [view, setView] = useState(0)
 
     const navigate = (e) => {
-        switch(e.target.id){
-        default:
-        case 'date_button_0':
-            setView(0)
-            break
-        case 'date_button_1':
-            setView(1)
-            break
-        case 'date_button_2':
-            setView(2)
-            break
-        }
+        let button_id = e.target.id.split('_')[2]
+        setView(parseInt(button_id))
     }
+
+    const buttons = [
+        { id: 'date_button_0', label: "Eingabe", permitted: true },
+        { id: 'date_button_1', label: "Übersicht", permitted: props.auth_level > 1},
+        { id: 'date_button_2', label: "manuelle Eingabe", permitted: props.auth_level > 2 }
+    ]
 
     return (
         <>
-            {props.auth_level > 1 ? <HeaderMenu>
-                <Button id='date_button_0' font_size={'1rem'} onClick={navigate}>Eingabe</Button>
-                <Button id='date_button_1' font_size={'1rem'} onClick={navigate}>Übersicht</Button>
-                {props.auth_level > 2 ? <Button id='date_button_2' font_size={'1rem'} onClick={navigate}>manuelle Eingabe</Button> : <></>}
-            </HeaderMenu> : <></>}
+            <HeaderMenu>
+                {buttons.map(({ id, label, permitted }) => {
+                    return (
+                        permitted && (
+                            <Button key={id} type='button' id={id} onClick={navigate}>
+                                {label}
+                            </Button>
+                        )
+                    )
+                })}
+            </HeaderMenu>
             <View view={view} fullname={props.fullname} theme={props.theme}/>
         </>
     )
 }
 
-// const AbsenceInput = styled(UnstyledAbsenceInput)`
-//     display: flex;
-//     flex-direction: row;
-//     align-items: center;
-//     flex-wrap: nowrap;
-
-//     @media screen and (max-width: 720px) {
-//         flex-direction: column;
-//         > select {
-//             width: 100%
-//         }
-//     }
-
-//     > select {
-//         max-height: 40px;
-//         padding: 5px;
-//         margin: 5px;
-//     }
-//     > img {
-//         padding: 2px;
-//         min-height: 21px;
-//         height: 10vh;
-//         max-height: 64px;
-//     }
-// `
-
 const View = (props) => {
-    switch(props.view){
-    default:
-    case 0:
-        return(<Suspense>
-            <AttendenceInput dates={dates} fullname={props.fullname} theme={props.theme}/>
-        </Suspense>)
-    case 1:
-        return(<Suspense><Overview dates={dates} theme={props.theme}/></Suspense>)
-    case 2:
-        return(<Suspense><AbsenceInput theme={props.theme}/></Suspense>)
+
+    const components = {
+        '0': <AttendenceInput fullname={props.fullname} theme={props.theme}/>,
+        '1': <Overview theme={props.theme}/>,
+        '2': <AbsenceInput theme={props.theme}/>
     }
+
+    return (
+        <Suspense fallback={<div>Lädt...</div>}>
+            {components[props.view]}
+        </Suspense>
+    )
 }
 
 export default Dateplanner
