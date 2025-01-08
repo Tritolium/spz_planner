ff0fcb708213da888c48e2c60485a763fa9f21a7
Tritolium <paracrafta@gmail.com>
2025-01-08 13:36:20 +0100

    Login: remove password info

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,7 +2,6 @@ import { useCallback } from "react"
 import StyledLogin from "./Login.styled"
 import { sha256 } from "js-sha256"
 
-// TODO: remove password info on 01/12/2024
 const serviceCell = process.env.REACT_APP_SERVICE_CELL
 const text = "Hallo, ich habe mein Passwort vergessen. Kannst du es mir bitte zurücksetzen?"
 
@@ -24,9 +23,8 @@ const Login = (props) => {
         <StyledLogin onSubmit={sendLogin}>
             <input id="loginname" type="text" placeholder="Namen eingeben"></input>
             <input id="passwd" type="password" placeholder="Passwort" aria-label="Passwort"></input>
-            <i>Passwort nur notwendig, wenn schon eins vergeben wurde, standardmäßig nicht der Fall</i>
             <button type="submit">Login</button>
-            <button onClick={pwrequest}>Passwort vergessen?</button>
+            <button id="pwreq" onClick={pwrequest}>Passwort vergessen?</button>
         </StyledLogin>
     )
 }
