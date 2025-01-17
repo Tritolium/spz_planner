b818624514dcda0d9d65c89509681d2247ec62d7
Tritolium <paracrafta@gmail.com>
2025-01-17 14:43:56 +0100

    AssociationAssignment: update interface, allow instrument input

--- src/components/administration/associationassignment/AssociationAssignment.js
+++ src/components/administration/associationassignment/AssociationAssignment.js
@@ -1,15 +1,19 @@
 import { useCallback, useEffect } from "react"
 import { useState } from "react"
-import { Check, Deny } from "../../../icons/Icons"
 import Button from "../../../modules/components/button/Button"
-import { Table } from "../../../modules/components/overview/Table"
-import { getAssociationAssignments, updateAssociationAssignments } from "../../../modules/data/DBConnect"
-import { StyledAssociationAssignment, StyledMobileTableHeader, StyledTableHeader } from "./AssociationAssignment.styled"
+import { getAssociationAssignments, getAssociations, host } from "../../../modules/data/DBConnect"
+import { StyledAssociationAssignment } from "./AssociationAssignment.styled"
+import Selector from "../../../modules/components/form/Selector"
+import SelectorItem from "../../../modules/components/form/SelectorItem"
+import Form from "../../../modules/components/form/Form"
 
 const AssociationAssignment = () => {
 
+    const [associations, setAssociations] = useState(new Array(0))
+    const [members, setMembers] = useState(new Array(0))
+    const [selected, setSelected] = useState(-1)
+
     const [associationassignment, setAssociationassignment] = useState(new Array(0))
-    const [changedAssignment, setChangedAssignment] = useState({})
 
     const fetchAssociationassignment = useCallback(async () => {
         getAssociationAssignments().then(assignments => {
@@ -18,83 +22,102 @@ const AssociationAssignment = () => {
         })
     }, [])
 
-    const onClick = useCallback((member_id, association_id, assigned) => {
-        let assign = {...changedAssignment}
-        if(assign[`${member_id}`] === undefined)
-            assign[`${member_id}`] = {}
-        
-        assign[`${member_id}`][`${association_id}`] = assigned
+    const fetchAssociations = useCallback(async () => {
+        let _associations = await getAssociations()
+        if(_associations !== undefined)
+            setAssociations(_associations)
+        else
+            setAssociations(new Array(0))
+    }, [])
 
-        setChangedAssignment(assign)
-    }, [changedAssignment])
+    const fetchMembers = useCallback(async () => {
+        fetch(`${host}/api/v0/member?api_token=${localStorage.getItem('api_token')}`)
+            .then(response => response.json())
+            .then(data => {
+                setMembers(data)
+            })
+    }, [])
 
-    const saveChanges = useCallback(() => {
-        updateAssociationAssignments(changedAssignment)
-    }, [changedAssignment])
+    const onSelect = useCallback((id) => {
+        setSelected(id)
+    }, [])
 
     useEffect(() => {
+        fetchAssociations()
         fetchAssociationassignment()
-    }, [fetchAssociationassignment])
+        fetchMembers()
+    }, [fetchAssociations, fetchAssociationassignment, fetchMembers])
 
     return(<StyledAssociationAssignment>
-        <Button onClick={saveChanges}>Speichern</Button>
-        <Table>
-            <thead>
-                <tr>
-                    <th>Name:</th>
-                    {associationassignment[0]?.Associations?.map(association => {
-                        return(
-                            <Header key={`h_${association.Association_ID}`} association={association}/>
-                        )
-                    })}
-                </tr>
-            </thead>
-            <tbody>
-                {associationassignment.map(member => {
-                    return(
-                        <tr key={member.Member_ID}>
-                            <td>{member.Fullname}</td>
-                            {member.Associations?.map(association => {
-                                return(
-                                    <Assignment key={`${association.Association_ID}_${member.Member_ID}`} member_id={member.Member_ID} association_id={association.Association_ID} initial={association.Assigned} onClick={onClick}/>
-                                )
-                            })}
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </Table>
+        <MemberSelector members={members} onSelect={onSelect} selected={selected} />
+        <AssociationAssignEditor associations={associations} member={associationassignment.find(member => member.Member_ID === selected)}/>
     </StyledAssociationAssignment>)
 }
 
-const Header = ({ association }) => {
+const MemberSelector = ({ members, onSelect, selected }) => {
     return (
-        <>
-            <StyledTableHeader>{association.Title}</StyledTableHeader>
-            <StyledMobileTableHeader>{association.Association_ID}</StyledMobileTableHeader>
-        </>
+        <Selector>
+            {members.map(member => {
+                return (
+                    <MemberItem key={member.Member_ID} member={member} onSelect={onSelect} selected={member.Member_ID === selected} />
+                )
+            })}
+        </Selector>
     )
 }
 
-const Assignment = ({ member_id, association_id, initial, onClick }) => {
+const MemberItem = ({ member, onSelect, selected }) => {
 
-    const [assigned, setAssigned] = useState(initial)
-
-    const callback = useCallback(() => {
-        let _assigned = !assigned
-        setAssigned(_assigned)
-        onClick(member_id, association_id, _assigned)
-    }, [assigned, member_id, association_id, onClick])
+    const onClick = useCallback(() => {
+        onSelect(member.Member_ID)
+    }, [onSelect, member.Member_ID])
 
     return (
-        <AssignButton assigned={assigned} callback={callback}/>
+        <SelectorItem className={selected ? "selected" : ""} onClick={onClick}>
+            {member.Forename} {member.Surname}
+        </SelectorItem>
     )
 }
 
-const AssignButton = ({ assigned, callback }) => {
-    return(
-        <td onClick={callback}>{assigned ? <Check /> : <Deny />}</td>
-    )
+const AssociationAssignEditor = ({ associations, member }) => {
+
+    const save = useCallback((e) => {
+        e.preventDefault()
+
+        let assignments = {}
+
+        for (let i = 0; i < associations.length; i++) {
+            let assign = document.getElementById(`assign_${associations[i].Association_ID}`).checked
+            let instrument = document.getElementById(`instrument_${associations[i].Association_ID}`).value
+            assignments[associations[i].Association_ID] = { assign, instrument }
+        }
+
+        fetch(`${host}/api/v0/member/${member.Member_ID}/associationassignment?api_token=${localStorage.getItem('api_token')}`, {
+            method: 'PUT',
+            body: JSON.stringify(assignments)
+        })
+    }, [associations, member])
+
+    useEffect(() => {
+        for (let i = 0; i < associations.length; i++) {
+            document.getElementById(`assign_${associations[i].Association_ID}`).checked = member?.Associations.find(assoc => assoc.Association_ID === associations[i].Association_ID).Assigned
+            document.getElementById(`instrument_${associations[i].Association_ID}`).value = member?.Associations.find(assoc => assoc.Association_ID === associations[i].Association_ID).Instrument
+        }
+    }, [associations, member])
+            
+
+    return (<>
+        <Form onSubmit={() => {}}>
+            {associations.map(association => {
+                return (<>
+                    <label>{association.Title}</label>
+                    <input id={`assign_${association.Association_ID}`} type="checkbox" defaultChecked={member?.Associations.find(assoc => assoc.Association_ID === association.Association_ID).Assigned}/>
+                    <input id={`instrument_${association.Association_ID}`} type="text" defaultValue={member?.Associations.find(assoc => assoc.Association_ID === association.Association_ID).Instrument}/>
+                </>)
+            })}
+            <Button onClick={save}>Speichern</Button>
+        </Form>
+    </>)
 }
 
 export default AssociationAssignment
--- src/components/administration/associationassignment/AssociationAssignment.styled.js
+++ src/components/administration/associationassignment/AssociationAssignment.styled.js
@@ -1,63 +1,23 @@
 import styled from "styled-components";
 
 export const StyledAssociationAssignment = styled.div`
-    display: flex;
-    flex-direction: column;
+    display: grid;
+    grid-template-columns: auto auto;
+    align-items: start;
     position: relative;
-    max-width: 100%;
-
-    img {
-        max-height: 1.5rem;
-    }
-
-    td {
-        text-align: center;
-        :nth-child(1) {
-            z-index: 2;
-        }
-    }
 
     button {
         width: fit-content;
         align-self: center;
     }
 
-    .IconWrapper {
-        max-height: 1.8rem;
-        max-width: 1.8rem;
-        position: absolute;
-        transform: translateX(-50%) translateY(-50%);
+    .selected {
+        background-color: ${({ theme }) => theme.primaryLight};
+        color: ${({ theme }) => theme.primaryDark};
     }
 
-    svg {
-        height: 100%;
-        width: 100%;
-    }
-`
-
-export const StyledTableHeader = styled.th`
-    display: table-cell;
-    
-    @media (max-width: ${({ theme }) => theme.mobile}) {
-        display: none;
-    }
-`
-
-export const StyledMobileTableHeader = styled.th`
-    display: none;
-    
-    @media (max-width: ${({ theme }) => theme.mobile}) {
-        display: table-cell;
-    }
-`
-
-export const StyledUsergroupLegend = styled.div`
-    display: none;
-
-    padding: 5px;
-    
-    @media (max-width: ${({ theme }) => theme.mobile}) {
-        display: flex;
-        flex-direction: column;
+    form {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
     }
 `
