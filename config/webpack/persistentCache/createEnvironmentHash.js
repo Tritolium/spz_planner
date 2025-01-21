b478c5f8da303a28d54e9fcfd6d44acf781aaa0a
Tritolium <paracrafta@gmail.com>
2025-01-21 16:40:12 +0100

    remove unused packages

--- config/webpack/persistentCache/createEnvironmentHash.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const { createHash } = require('crypto');
-
-module.exports = env => {
-  const hash = createHash('sha256');
-  hash.update(JSON.stringify(env));
-
-  return hash.digest('hex');
-};
