5ec1c0fc73532f5ca257b8ef914e74a6022ba317
Tritolium <paracrafta@gmail.com>
2025-01-24 09:43:59 +0100

    dateplanner: Overview: add loading animation

--- src/components/dateplanner/overview/Overview.jsx
+++ src/components/dateplanner/overview/Overview.jsx
@@ -9,6 +9,8 @@ import { Alert, Blank, Check, Deny, PlusOne } from "../attendenceInput/Terminzus
 import { IoReload } from "react-icons/io5"
 import { OverviewTable } from "./OverviewTable"
 import { hasPermission } from "../../../modules/helper/Permissions"
+import { ImSpinner10 } from "react-icons/im"
+import pedro from "../../../modules/img/racoon.gif"
 
 const Overview = ({ theme }) => {
 
@@ -18,6 +20,8 @@ const Overview = ({ theme }) => {
     const [attendences, setAttendences] = useState(new Array(0))
     const [evaluation, setEvaluation] = useState(new Array(0))
 
+    const [loading, setLoading] = useState(false)
+
     const fetchUsergroups = async () => {
         let _usergroups = await getOwnUsergroups()
         _usergroups = _usergroups.filter(usergroup => hasPermission(7, usergroup.Association_ID))
@@ -25,8 +29,10 @@ const Overview = ({ theme }) => {
     }
 
     const fetchAttendences = useCallback(async () => {
+        setLoading(true)
         let _attendences = await getAllAttendences(selectedUsergroup_ID)
         setAttendences(_attendences)
+        setLoading(false)
     }, [selectedUsergroup_ID])
 
     const fetchEval = useCallback(async () => {
@@ -55,14 +61,15 @@ const Overview = ({ theme }) => {
         setSelectedUsergroup_ID(usergroups[0]?.Usergroup_ID)
     }, [usergroups])
 
-    if(attendences.length === 0){
-        return(
+    if(attendences.length === 0 || loading){
+        return(<StyledOverview>
             <select name="usergroup" id="usergroup_select" onChange={onUsergroupChange}>
                 {usergroups.map((usergroup, index) => {
                     return(<option key={index} value={usergroup.Usergroup_ID}>{usergroup.Title}</option>)
                 })}
             </select>
-        )
+            {loading ? theme.pedro ? <img src={pedro} className="pedro" alt="Loading" /> : <ImSpinner10 className="spinner"/> : <></>}
+        </StyledOverview>)
     } else {
         return(
             <StyledOverview>
@@ -143,7 +150,7 @@ const EvalRow = ({ event, attendences, instruments, theme }) => {
 
     const getInstruments = (attendences, attendence) => {
         let _instruments = {}
-        let _attendences
+        let _attendences = []
         if (attendence === -1)
             _attendences = attendences?.filter(att => att.Attendence === attendence && att.Prediction === 0)
         else
@@ -169,6 +176,8 @@ const EvalRow = ({ event, attendences, instruments, theme }) => {
     }
 
     useEffect(() => {
+        if (attendences === undefined)
+            return
         setProbAttendingInstruments(getInstruments(attendences, -1))
         setAttendingInstruments(getInstruments(attendences, 1))
     }, [attendences])
